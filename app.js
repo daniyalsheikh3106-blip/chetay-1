4d185b34e27557fda6840ce8a52d1ff2e1c476d3
daniyalsheikh3106-blip <daniyalsheikh3106@gmail.com>
2025-11-24 23:22:23 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+var a = 10;
+
+document.write("Result:<br>");
+document.write("The value of a is: " + a + "<br><br>");
+document.write("..................................................<br><br>");
+
+document.write("The value of ++a is: " + (++a) + "<br>");
+document.write("Now the value of a is: " + a + "<br><br>");
+
+document.write("The value of a++ is: " + (a++) + "<br>");
+document.write("Now the value of a is: " + a + "<br><br>");
+
+document.write("The value of --a is: " + (--a) + "<br>");
+document.write("Now the value of a is: " + a + "<br><br>");
+
+document.write("The value of a-- is: " + (a--) + "<br>");
+document.write("Now the value of a is: " + a + "<br><br>");
